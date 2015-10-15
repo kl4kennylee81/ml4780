813387dd105b06f2c2f1c00d127f1db04add8562
Kenneth Lee <kl488@cornell.edu>
2015-10-15 03:03:39 -0400

    hi

--- hw04/hinge.m
+++ hw04/hinge.m
@@ -18,26 +18,26 @@ loss = 0;
 gradient = rand(d,1);
 
 
-loss_sum = 0;
+% loss_sum = 0;
 
-for i = 1:n	
-	loss_sum = loss_sum + max( 1 - (yTr(i)*(w.'*xTr(:,i)) ),0 );
-end
+% for i = 1:n	
+% 	loss_sum = loss_sum + max( 1 - (yTr(i)*(w.'*xTr(:,i)) ),0 );
+% end
 
-loss = loss_sum + lambda*w.'*w;
+% loss = loss_sum + lambda*w.'*w;
 
 
-gradient_loss_sum = 0;
+% gradient_loss_sum = 0;
 
-for i = 1:n
-	val = yTr(i) * (w.'*xTr(:,i));
+% for i = 1:n
+% 	val = yTr(i) * (w.'*xTr(:,i));
 
-	if val < 1
-		gradient_loss_sum = gradient_loss_sum + (-yTr(i)) * xTr(:,i)
-	end
-end
+% 	if val < 1
+% 		gradient_loss_sum = gradient_loss_sum + (-yTr(i)) * xTr(:,i)
+% 	end
+% end
 
-gradient = gradient_loss_sum + 2*lambda*w
+% gradient = gradient_loss_sum + 2*lambda*w
 
 
 
