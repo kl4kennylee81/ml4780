9392d8d641ba2a3965a2a5ce56868150580afe33
Hong <hongj77@gmail.com>
2015-11-25 20:46:03 -0500

    ace d portgas

--- hw06/crossvalidate.m
+++ hw06/crossvalidate.m
@@ -19,14 +19,45 @@ function [bestC,bestP,bestval,allvalerrs]=crossvalidate(xTr,yTr,ktype,Cs,paras)
 %
 
 
-%% Split off validation data set
-% YOUR CODE
+[d,n] = size(xTr);
 
+size_validation = ceil(n/5);
+size_training = n - size_validation;
 
-%% Evaluate all parameter settings
-% YOUR CODE
+xTr_validation = xTr(:, 1:size_validation);
+yTr_validation = yTr(:, 1:size_validation);
 
-%% Identify best setting
-% YOUR CODE
+xTr_training = xTr(:, 1:size_training);
+yTr_training = yTr(:, 1:size_training);
 
+C_len = length(Cs);
+P_len = length(paras);
+errorMatrix(1:C_len, 1:P_len) = 0; % error matrix initialized to 0
+
+minError = 101;
+minC = -1;
+minP = -1;
+
+for i = 1:C_len
+	for j=1:P_len
+		c = Cs(i);
+		kpar = paras(j);
+		[svmclassify, sv_i, alphas] = trainsvm(xTr_training, yTr_training, c, ktype, kpar);
+		guess = svmclassify(xTr_validation); % guess is m x 1
+
+		err = sum(sign(guess) ~= yTr_validation.') ./ length(guess);
+		errorMatrix(i,j) = err;
+
+		if err < minError
+			minError = err;
+			minC = c;
+			minP = kpar;
+		end;
+	end;
+end;
+
+bestC = minC;
+bestP = minP;
+bestval = minError;
+allvalerrs = errorMatrix;
 
