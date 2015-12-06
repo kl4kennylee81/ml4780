0bb512f581acd6a0515408403a1e1012f0c265fd
Hong <hongj77@gmail.com>
2015-12-06 05:53:15 -0500

    vidit

--- hw07/forest.m
+++ hw07/forest.m
@@ -8,6 +8,15 @@ function F=forest(x,y,nt)
 % OUTPUT:
 % F | Forest
 %
+[d, n] = size(x);
 
+F = {};
+for i=1:nt
+	%select n with replacement
+	all_index = (1:n);
+	rand_index = randsample(all_index,n,replacement = true);
+	xTr_rand = xTr(rand_index);
+	yTr_rand = yTr(rand_index);
+	F(i) = id3tree(xTr_rand,yTr_rand);
+end
 %% fill in code here
-
